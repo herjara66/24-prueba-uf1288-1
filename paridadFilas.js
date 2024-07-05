e28b6c8a682ae5d51da8adf3003812c59d98c12c
herjara66 <jaramillojesush@gmail.com>
2024-07-05 11:05:33 +0200

    se realiza una funcion que compare la paridad de las filas, se utilizo metodo join para comparar

---
+++ paridadFilas.js
@@ -0,0 +1,23 @@
+const bytes = [
+    [1, 0, 0, 1, 1, 1, 0, 1],
+    [0, 1, 1, 0, 0, 1, 0, 1],
+    [0, 0, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 0, 1, 0, 0],
+    [1, 1, 0, 0, 1, 0, 1, 0],
+    [0, 1, 1, 1, 0, 0, 1, 0],
+    [1, 0, 1, 0, 0, 1, 1, 1],
+    [0, 0, 1, 1, 0, 0, 0, 0],
+  ];
+  
+  function bytes_a_comprobar(fila) {
+    let paridad = 0;
+    for (let bit of fila) {
+      paridad ^= bit; 
+    }
+    return paridad % 2 === 0;
+  }
+  
+  for (const fila of bytes) {
+    const esPar = bytes_a_comprobar(fila);
+    console.log(`Fila: ${fila.join(', ')} - Paridad: ${esPar ? 'Par' : 'Impar'}`);
+  }
